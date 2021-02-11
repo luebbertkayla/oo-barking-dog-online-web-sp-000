e056adb4cd963eec5d2ce502cb94a800832668f0
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-11 02:35:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog 
+  def bark
+    puts "woof!"
+  end
+  
+  def name=(dog_name)
+    @specific_dog_name = dog_name
+  end
+  
+  def name
+    @specific_dog_name
+  end
+end
